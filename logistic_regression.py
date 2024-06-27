caec3b92125bfa01a4c11fb126f18042cf4c5eff
JasonYRChen <jkmylife@gmail.com>
2024-06-27 11:24:17 +0800

    (logistic_regression) add multinomial Logistic Regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -69,40 +69,114 @@ class LogisticRegression:
         return y_hat
 
 
+class LogisticRegressionMultinomial:
+    def __init__(self, iteration=1000, learning_rate=0.001, error_rate=0.001):
+        self.iteration = iteration
+        self.learning_rate = learning_rate
+        self.error_rate = error_rate
+        self.errors = []
+        self.coef = None
+
+    @staticmethod
+    def sigmoid(X, coef):
+        y_hat = np.exp(X @ coef)
+        sum_y_hat = np.sum(y_hat, axis=1).reshape(-1, 1)
+        y_hat = np.divide(y_hat, sum_y_hat)
+        return y_hat
+
+    def fit(self, X, y, add_intercept=True):
+        # y needs to be integer array and starts from 0 to K-1 one by one.
+        # y needs to be flat.
+
+        if add_intercept: # add constant at the first column of X
+            X = np.hstack((np.ones((X.shape[0], 1)), X))
+
+        K = len(set(y)) # numbers of classes in y
+        coefs = np.zeros((X.shape[1], K))
+        ones = np.zeros((X.shape[0], K)) # dummies
+        for i in range(K):
+            ones[y == i, i] = 1
+
+        for _ in range(self.iteration):
+            y_hat = self.sigmoid(X, coefs)
+            
+            error = np.sum(np.argmax(y_hat, axis=1) != y) / len(y)
+            self.errors.append(error)
+            if error <= self.error_rate: # meet maximum error criterion
+                break
+
+            coefs += self.learning_rate * X.T @ (ones - y_hat)
+
+        self.coef = coefs
+
+    def predict(self, x, add_intercept=True):
+        if add_intercept:
+            x = np.hstack((np.ones((x.shape[0], 1)), x))
+
+        y_hat = self.sigmoid(x, self.coef)
+        y_hat = np.argmax(y_hat, axis=1)
+        return y_hat
+
+
 if __name__ == '__main__':
-    datapoints = 100
+    datapoints = 1000
+    features = 1
+    classes = 4
     random_radius = 0.2
-    iteration = 2000
+    iteration = 4000 
     learning_rate = 0.01
     error_rate = 0.01
 
-    radius = int(datapoints * random_radius)
-    mid = int(datapoints / 2)
-    X = np.linspace(0, 10, datapoints).reshape(-1, 1)
-    XX = np.hstack((np.ones((X.shape[0], 1)), X))
+    # data for y classes > 2
+    X = np.random.rand(datapoints, features) * 10
     y = np.zeros(datapoints)
-    y[mid:] = 1
-#    y = np.ones(datapoints)
-#    y[mid:] = 0
-    np.random.shuffle(y[mid-radius: mid+radius])
-    y = y.reshape(-1 ,1)
 
-    # fit and predict (maximum likelihood)
-    l = LogisticRegression(iteration, learning_rate, error_rate)
+    x = np.sum(X, axis=1)
+    indices = x.argsort()
+    interval = int(datapoints / classes)
+    for i in range(1, classes):
+        y[x >= x[indices[interval * i]]] = i
+
+    # for classes == 2
+#    radius = int(datapoints * random_radius)
+#    mid = int(datapoints / 2)
+#    X = np.linspace(0, 10, datapoints).reshape(-1, 1)
+#    XX = np.hstack((np.ones((X.shape[0], 1)), X))
+#    y = np.zeros(datapoints)
+#    y[mid:] = 1
+##    y = np.ones(datapoints)
+##    y[mid:] = 0
+#    np.random.shuffle(y[mid-radius: mid+radius])
+
+    # multinomial logistic regression
+    l = LogisticRegressionMultinomial(iteration, learning_rate, error_rate)
     l.fit(X, y)
     y_hat = l.predict(X)
-    print(f'error (ML): {np.sum(y != y_hat) / len(y)}, coef: {l.coef.T}')
+    error = np.sum(y_hat != y) / len(y)
+
+    print(f'final error: {l.errors[-1]}, error: {error}')
+    plt.plot(range(len(l.errors)), l.errors)
+
+    # binary logistic regression: fit and predict (maximum likelihood)
+#    l = LogisticRegression(iteration, learning_rate, error_rate)
+#    l.fit(X, y.reshape(-1, 1))
+#    y_hat = l.predict(X)
+#    error = np.sum(y_hat != y.reshape(-1, 1)) / len(y)
+#    print(f'error (ML): {l.errors[-1]}, error: {error}')
+
+#    plt.figure('old')
+#    plt.plot(range(len(l.errors)), l.errors)
 
     # fit and predict (linear)
-    coef = logistic_regression_linear(X, y)
-    y_linear = np.where(l.sigmoid(XX, coef) >= 0.5, 1, 0)
-    print(f'error (linear): {np.sum(y != y_linear) / len(y)}, coef: {coef.T}')
+#    coef = logistic_regression_linear(X, y)
+#    y_linear = np.where(l.sigmoid(XX, coef) >= 0.5, 1, 0)
+#    print(f'error (linear): {np.sum(y != y_linear) / len(y)}, coef: {coef.T}')
 
     # plot
-    sigmoid_ML = l.sigmoid(XX, l.coef)
-    sigmoid_linear = l.sigmoid(XX, coef)
-    plt.plot(X, sigmoid_ML)
-    plt.plot(X, sigmoid_linear, 'g')
-    plt.scatter(X, y, c='r', s=0.1)
+#    sigmoid_ML = l.sigmoid(XX, l.coef)
+#    sigmoid_linear = l.sigmoid(XX, coef)
+#    plt.plot(X, sigmoid_ML)
+#    plt.plot(X, sigmoid_linear, 'g')
+#    plt.scatter(X, y, c='r', s=0.1)
 
     plt.show()
